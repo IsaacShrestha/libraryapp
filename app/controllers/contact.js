f068b1b8e46221ddb86c1498e4cce6c40e45eb38
Isaac Shrestha <ishrestha@unomaha.edu>
2016-11-10 20:29:09 -0600

    contact form done

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -2,9 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	senderEmail: '',
-	message: '',
+	senderMessage: '',
 
 	
+	isValidEmail: Ember.computed.match('senderEmail', /^.+@.+\..+$/),
+	isMessageEnoughLong: Ember.computed.gte('senderMessage.length', 5),
+	isDisabled: Ember.computed.not('isMessageEnoughLong'), //not able to validate more than one field
 
-	isDisabled: Ember.computed.empty('senderEmail');
+	actions: {
+		receiveMessage: function(){
+			alert('Your message with e-mail address '+this.get('senderEmail')+' received');
+			this.set('responseMessage', 'Receieved message successfully with e-mail address '+this.get('senderEmail'));
+			this.set('senderEmail', '');
+			this.set('senderMessage', '');
+		}
+	}
+
+	
 });
